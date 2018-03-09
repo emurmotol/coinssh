7f998e65019d2dac955362a6f076d7d0a3af402f
Emur Motol <klarizonemarmotol@ninepinetech.com>
2018-03-09 15:14:21 +0800

    remaster

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,8 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
+require("popper.js/dist/popper.min.js");
 require("bootstrap/dist/js/bootstrap.bundle.js");
+require("datatables.net/js/jquery.dataTables.js");
+require("datatables.net-bs4/js/dataTables.bootstrap4.js");
 $(() => {
 
 });
