770c58216b4f52dc953f750feee791ab4439a9de
Emur Motol <emurmotol@gmail.com>
2018-03-10 14:09:00 +0800

    hating ui

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,8 +1,10 @@
+require("jquery/dist/jquery.js");
 require("expose-loader?$!expose-loader?jQuery!jquery");
-require("popper.js/dist/popper.min.js");
-require("bootstrap/dist/js/bootstrap.bundle.js");
-require("datatables.net/js/jquery.dataTables.js");
+require("popper.js/dist/popper.js");
+require("bootstrap/dist/js/bootstrap.js");
+require("datatables/media/js/jquery.dataTables.js");
 require("datatables.net-bs4/js/dataTables.bootstrap4.js");
+// require("startbootstrap-sb-admin/js/sb-admin.js");
 $(() => {
 
 });
