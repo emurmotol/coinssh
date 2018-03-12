d77ccb93e9ff8440af3f1b49aab884209fc2dc20
Emur Motol <klarizonemarmotol@ninepinetech.com>
2018-03-12 14:53:38 +0800

    added seeder

--- grifts/db.go
+++ grifts/db.go
@@ -2,13 +2,60 @@ package grifts
 
 import (
 	"github.com/markbates/grift/grift"
+	"github.com/manveru/faker"
+	"github.com/emurmotol/coinssh/models"
+	"github.com/pkg/errors"
 )
 
 var _ = grift.Namespace("db", func() {
 
-	grift.Desc("seed", "Seeds a database")
+	grift.Add("seed:admin", func(c *grift.Context) error {
+
+		admin := &models.User{
+			Name: "Administrator",
+			Email: "su@coinssh.com",
+			Password: "su@coinssh.com",
+		}
+
+		return models.DB.Create(admin)
+	})
+
+	grift.Add("seed:users", func(c *grift.Context) error {
+		fake, err := faker.New("en")
+
+		if err != nil {
+			return err
+		}
+
+		for i := 0; i <= 20; i++ {
+			user := &models.User{
+				Name: fake.Name(),
+				Email: fake.Email(),
+				Password: "secret123",
+			}
+
+			if err := models.DB.Create(user); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
 	grift.Add("seed", func(c *grift.Context) error {
-		// Add DB seeding stuff here
+
+		if err := models.DB.TruncateAll(); err != nil {
+			return errors.WithStack(err)
+		}
+
+		if err := grift.Run("db:seed:admin", c); err != nil {
+			return errors.WithStack(err)
+		}
+
+		if err := grift.Run("db:seed:users", c); err != nil {
+			return errors.WithStack(err)
+		}
+
 		return nil
 	})
 
