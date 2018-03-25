484dbca05f595500b1d9057438694abc72f34848
Emur Motol <emurmotol@gmail.com>
2018-03-25 15:02:11 +0800

    migrated to cdn

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -1,10 +1,13 @@
-// require("jquery/dist/jquery.js");
-require("expose-loader?$!expose-loader?jQuery!jquery");
+// require("expose-loader?$!expose-loader?jQuery!jquery");
 // require("popper.js/dist/popper.js");
-require("bootstrap/dist/js/bootstrap.bundle.js");
-// require("datatables/media/js/jquery.dataTables.js");
+// require("bootstrap/dist/js/bootstrap.bundle.js");
+// require("chart.js/dist/Chart.js");
+// require("datatables.net/js/jquery.dataTables.js");
 // require("datatables.net-bs4/js/dataTables.bootstrap4.js");
 // require("startbootstrap-sb-admin/js/sb-admin.js");
+// require("startbootstrap-sb-admin/js/sb-admin-datatables.js");
+// require("startbootstrap-sb-admin/js/sb-admin-charts.js");
+
 $(() => {
 
 });
